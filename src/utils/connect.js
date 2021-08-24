bac2ec53b1e4ff15e6fee422878db6cd48596fec
Martin Alix <martin.alix@autodesk.com>
2021-08-24 15:29:25 -0400

    Version 2108241530

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -2,10 +2,24 @@
 
 const mongoose = require('mongoose')
 
-module.exports = (dbUrl) => mongoose.connect(dbUrl, {
-	useFindAndModify: false,
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-	connectTimeoutMS: 60000,
+module.exports = (dbUrl) => new Promise((done) => {
+	console.log('[Database] Connecting...')
+	mongoose.connect(dbUrl, {
+		useFindAndModify: false,
+		reconnectTries: Number.MAX_VALUE,
+		useNewUrlParser: false,
+		useCreateIndex: true,
+		useUnifiedTopology: false,
+		connectTimeoutMS: 10000,
+	}, (error) => {
+		if (error) {
+			console.error('[ERROR] Connect error', { error })
+			if (process.env.NODE_ENV === 'production') {
+				process.exit(-1)
+			}
+			return
+		}
+		console.log('[Database] Connected') //
+		done()
+	})
 })
